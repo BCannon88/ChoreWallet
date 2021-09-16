7706720d60e548dc6eac65a51fdb0e8276d29a86
William Takes <william.e.takes@vanderbilt.edu>
2021-09-15 19:37:44 -0500

    navbar started

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {Router} from "react-router-dom";
-import Login from "./components/login"
+import Login from "./components/login";
+import Nav from './components/Nav';
 
 export const App = () => {
 
   return (
     <Router>
       <div>
-
+        <Nav />
           <div path="/login">
             <Login />
           </div>
